26543b261b7473803fc158d240649cbef151bb55
Branimir Djordjevic <branimir1111@gmail.com>
2025-09-03 18:14:13 +0200

    asdjklasjkl

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -25,7 +25,7 @@ export default function TabTwoScreen() {
         <ThemedText type="title">Explore</ThemedText>
       </ThemedView>
       <ThemedText>
-        This app includes example code to help you get started.
+        This app includes example code to help you get startedddddd.
       </ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
